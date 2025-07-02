955ee764cc2f45d2d54e3fe91b75001701f7e584
Davide Fedeli <davidefedeli8899@gmail.com>
2025-07-02 16:31:27 +0200

    feat: paint on texture

--- src/ProceduralGenerationMap/Assets/Scripts/InteractableMap.cs
+++ src/ProceduralGenerationMap/Assets/Scripts/InteractableMap.cs
@@ -12,15 +12,14 @@ using UnityEngine;
             var mousePos = Input.mousePosition;
             Ray ray = Camera.main.ScreenPointToRay(mousePos);
             Physics.Raycast(ray , out RaycastHit hit);
-            Debug.Log(hit.textureCoord);
-            Vector2 pixelPosition = new Vector2(hit.textureCoord.x , hit.textureCoord.y);
+            //Debug.Log(hit.textureCoord);
+            
+            Vector2Int pixelPosition = new Vector2Int((int)(hit.textureCoord.x  *100), (int)(hit.textureCoord.y * 100));
             if (Input.GetMouseButtonDown(0))
             {
-                foreach (Color pixel in _texture.GetPixels())
-                {
-                    pixel = new Color(0, 0, 0, 0);
-                }
-                _texture.SetPixel((int)pixelPosition.x, (int)pixelPosition.y, Color.red);
+                
+                _texture.SetPixel(pixelPosition.x,pixelPosition.y, Color.red);
+                _texture.Apply();
             }
         }
 
